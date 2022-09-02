a250fe30d925cb1bfff245bb930066b955bb1fce
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2022-09-02 16:57:08 -0400

    Update WebAppTest.cs

--- WebAppTest.cs
+++ WebAppTest.cs
@@ -1,66 +1,68 @@
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 [Parallelizable(ParallelScope.Self)]
 public class Tests : PageTest
 {
-    [Test]
-    public async Task HomepageHasWebTestPlaywrightInTitleAndAButtonWithLinkToContactForm()
+    public static string webAppUrl;
+
+    [OneTimeSetUp]
+    public void Init()
     {
-        await Page.GotoAsync("https://localhost:7108/");
-        await Expect(Page).ToHaveTitleAsync(new Regex("WebTestPlaywright"));
+        webAppUrl = TestContext.Parameters["WebAppUrl"] 
+            ?? throw new Exception("WebAppUrl is not configured as a parameter.");
+    }
+
+
+    [Test]
+    public async Task Clicking_ContactButton_Goes_To_ContactForm()
+    {
+        await Page.GotoAsync(webAppUrl);
         var formButton = Page.Locator("text=Open Contact Form");
         await formButton.ClickAsync();
         await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Form"));
     }
 
     [Test]
-    public async Task FormEnteringANameWithLessThanThreeCharactersExpectingThanksPage()
+    public async Task Filling_And_Submitting_ContactForm_Goes_To_SuccessPage()
     {
-        await Page.GotoAsync("https://localhost:7108/Home/Form");
-        await Page.Locator("#Form_FirstName").FillAsync("Néstor");
-        await Page.Locator("#Form_LastName").FillAsync("Campos");
-        await Page.Locator("#Form_EmailAddress").FillAsync("nestor@gmail.com");
-        await Page.Locator("#Form_BirthDate").FillAsync("1989-03-16");
-        await Page.Locator("input.btn").ClickAsync();
+        await Page.GotoAsync($"{webAppUrl}/Home/Form");
+        await Page.Locator("text=First name").FillAsync("Néstor");
+        await Page.Locator("text=Last name").FillAsync("Campos");
+        await Page.Locator("text=Email address").FillAsync("nestor@gmail.com");
+        await Page.Locator("text=Birth date").FillAsync("1989-03-16");
+        await Page.Locator("text=Send").ClickAsync();
         await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Success"));
     }
 
     [Test]
-    public async Task FormEnteringALastNameWithLessThanThreeCharactersExpectingThanksPage()
+    public async Task Filling_And_Submitting_ContactForm_With_InvalidEmail()
     {
-        await Page.GotoAsync("https://localhost:7108/Home/Form");
-        await Page.Locator("#Form_FirstName").FillAsync("Néstor");
-        await Page.Locator("#Form_LastName").FillAsync("Campos");
-        await Page.Locator("#Form_EmailAddress").FillAsync("nestor@gmail.com");
-        await Page.Locator("#Form_BirthDate").FillAsync("1989-03-16");
-        await Page.Locator("input.btn").ClickAsync();
-        await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Success"));
+        await Page.GotoAsync($"{webAppUrl}/Home/Form");
+        await Page.Locator("text=First name").FillAsync("Néstor");
+        await Page.Locator("text=Last name").FillAsync("Campos");
+        await Page.Locator("text=Email address").FillAsync("nestorgmail.com");
+        await Page.Locator("text=Birth date").FillAsync("1989-03-16");
+        await Page.Locator("text=Send").ClickAsync();
+        await Expect(Page).ToHaveURLAsync($"{webAppUrl}/Home/Form");
+        await Expect(Page.Locator("text=The Email address field is not a valid e-mail address.")).ToBeVisibleAsync();
+        
     }
 
     [Test]
-    public async Task FormEnteringAnEmailWithLessThanThreeCharactersExpectingThanksPage()
+    public async Task Filling_And_Submitting_ContactForm_With_InvalidBirthdate()
     {
-        await Page.GotoAsync("https://localhost:7108/Home/Form");
-        await Page.Locator("#Form_FirstName").FillAsync("Néstor");
-        await Page.Locator("#Form_LastName").FillAsync("Campos");
-        await Page.Locator("#Form_EmailAddress").FillAsync("nestor@gmail.com");
-        await Page.Locator("#Form_BirthDate").FillAsync("1989-03-16");
-        await Page.Locator("input.btn").ClickAsync();
-        await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Success"));
+        await Page.GotoAsync($"{webAppUrl}/Home/Form");
+        await Page.Locator("text=First name").FillAsync("Néstor");
+        await Page.Locator("text=Last name").FillAsync("Campos");
+        await Page.Locator("text=Email address").FillAsync("nestor@gmail.com");
+        await Page.Locator("text=Birth date").FillAsync("2023-03-16");
+        await Page.Locator("text=Send").ClickAsync();
+        await Expect(Page).ToHaveURLAsync($"{webAppUrl}/Home/Form");
+        await Expect(Page.Locator("text=The birth date is invalid.")).ToBeVisibleAsync();
     }
 
-    [Test]
-    public async Task FormEnteringAValidDateWithLessThanThreeCharactersExpectingThanksPage()
-    {
-        await Page.GotoAsync("https://localhost:7108/Home/Form");
-        await Page.Locator("#Form_FirstName").FillAsync("Néstor");
-        await Page.Locator("#Form_LastName").FillAsync("Campos");
-        await Page.Locator("#Form_EmailAddress").FillAsync("nestor@gmail.com");
-        await Page.Locator("#Form_BirthDate").FillAsync("2020-03-16");
-        await Page.Locator("input.btn").ClickAsync();
-        await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Success"));
-    }
 }
